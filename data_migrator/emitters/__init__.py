20a0a7e63725c945ed18adc450ca0e979dd7dccc
Ilja Heitlager <iheitlager@schubergphilis.com>
2017-04-17 17:20:48 +0200

    update doc string

--- data_migrator/emitters/__init__.py
+++ data_migrator/emitters/__init__.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-"""
-This module contains all classes for models, managers and fields
+"""Emitters are used to export models to output format.
+
+This module contains all classes for emitters: base and actuals
 
 * :class:`BaseEmitter`
 * :class:`MySQLEmitter`
-* ...
+* :class:`CSVEmitter`
 """
 
 from .mysql import MySQLEmitter
