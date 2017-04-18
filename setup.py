b498c258eebf5e0555edaea208a2361bc0845c89
Ilja Heitlager <iheitlager@schubergphilis.com>
2017-04-18 22:17:06 +0200

    adding CONTRIBUTING.md

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.rst", "r") as f:
 
 setup(
     name='data-migrator',
-    version="0.4.6",
+    version="0.4.7alpha0",
     description='declarative data migration and transformation package',
     long_description=long_description,
     license='MIT',
