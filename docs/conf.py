4a935a79342ec92bf37172ecbe8e017361875a18
Ilja Heitlager <iheitlager@schubergphilis.com>
2017-04-22 17:23:50 +0200

    Bump version: 0.5.0 → 0.5.1alpha0

--- docs/conf.py
+++ docs/conf.py
@@ -63,7 +63,7 @@ author = u'Ilja Heitlager'
 # built documents.
 #
 # The full version, including alpha/beta/rc tags.
-release = "0.5.0"
+release = "0.5.1alpha0"
 # The short X.Y version.
 version = ".".join(release.split(".")[0:2])
 
