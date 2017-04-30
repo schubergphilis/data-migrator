179afde88a243bc4d3b5ce7d4e3e051b7a5a8382
Ilja Heitlager <iheitlager@schubergphilis.com>
2017-04-30 08:57:55 +0200

    Simplify semver (#38)
    * bumpversion -> dev #37 
    * normalize version string
    
    * propagated release label change

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.rst", "r") as f:
 
 setup(
     name='data-migrator',
-    version="0.5.1alpha0",
+    version="0.5.1.dev0",
     description='declarative data migration and transformation package',
     long_description=long_description,
     license='MIT',
