f63438938116c88925f11a15849f45bed754a72f
Ilja Heitlager <iheitlager@schubergphilis.com>
2017-08-18 17:59:09 +0200

    Bump version: 0.6.0.dev0 → 0.6.0 (#61)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.rst", "r") as f:
 
 setup(
     name='data-migrator',
-    version="0.6.0.dev0",
+    version="0.6.0",
     description='declarative data migration and transformation package',
     long_description=long_description,
     license='MIT',
