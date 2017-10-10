6d11d540a5b4bc2e9d09cceb400943a5939006f0
Ilja Heitlager <iheitlager@schubergphilis.com>
2017-10-10 21:36:58 +0200

    new drop

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.rst", "r") as f:
 
 setup(
     name='data-migrator',
-    version="0.6.2.dev1",
+    version="0.6.2.dev2",
     description='declarative data migration and transformation package',
     long_description=long_description,
     license='MIT',
