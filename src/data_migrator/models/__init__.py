191433de549530948e26f7f426eb2eb567f6f1ee
Ilja Heitlager <iheitlager@schubergphilis.com>
2017-10-24 18:20:03 +0200

    Update __init__.py

--- src/data_migrator/models/__init__.py
+++ src/data_migrator/models/__init__.py
@@ -11,10 +11,10 @@ This module contains all classes for models, managers and fields
 
 from .base import Model  # noqa
 from .manager import BaseManager, SimpleManager  # noqa
-from .fields import (IntField, IntegerField, NullIntField, #noqa
-                     StringField, NullStringField, #noqa
+from .fields import (IntField, IntegerField, NullIntField, # noqa
+                     StringField, NullStringField, # noqa
                      BooleanField, NullField, UUIDField,  # noqa
                      JSONField, MappingField, HiddenField,  # noqa
                      DateTimeField, UTCNowField, # noqa
-                     ArrayField, ListField, DictField, ObjectField,  #noqa
+                     ArrayField, ListField, DictField, ObjectField,  # noqa
                      ModelField)  # noqa
